5057a7406694ce5b5dea8a1fb4290d15c7216d84
edd-2 <EgorDanyluk72@mail.ru>
2021-11-17 08:57:03 +0700

    ViewModel 0.0.1

---
+++ app/src/main/java/com/example/esp32app/screens/MenuFragmentViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.esp32app.screens
+
+import androidx.lifecycle.ViewModel
+
+class MenuFragmentViewModel : ViewModel() {
+
+
+    fun sendtext(s: String): String{
+        return s
+    }
+
+}
